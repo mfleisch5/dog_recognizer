c13fc97aed6d9a7994df6df630bb8870715193f0
Michael Fleischmann <mfleisch50@gmail.com>
2018-02-10 00:50:40 -0500

    Running neural network

--- api_mgr.py
+++ api_mgr.py
@@ -1,17 +1,30 @@
-import indicoio
+import indicoio, os, base64
 from indicoio.custom import Collection
 indicoio.config.api_key = '5c6a25f102276fa6acbf0a5ac79548a5'
 
 collection = Collection("dogs")
 
 # Add Data
-collection.add_data([["text1", "label1"], ["text2", "label2"], ...])
+""""
+for dir in os.listdir('Images'):
+    pics, count = [], 0
+    for file in os.listdir('Images/' + dir):
+        print('Running:', dir, file)
+        pics.append([os.path.abspath('Images/' + dir + '/' + file), dir])
+        count += 1
+        if count == 5:
+            break
+    collection.add_data(pics)
+
+
 
 # Training
 collection.train()
 
 # Telling Collection to block until ready
 collection.wait()
+"""
 
-# Done! Start analyzing text
-collection.predict("indico is so easy to use!")
+
+print(*sorted(collection.predict(os.path.abspath('Images/Yorkshire_terrier/n02094433_4005.jpg')).items(), reverse=True,
+              key=lambda p: p[1]), sep='\n')
