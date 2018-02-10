440665f30a92b5e232f3598226880faa9f26b217
Becker Ewing <jbewing@live.com>
2018-02-10 19:41:53 +0000

    Configured Flask

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def index():
     return 'Hello dogs'
 
 
-app.debug = True
+application.debug = True
+
+if __name__ == '__main__':
+    application.run()
+
